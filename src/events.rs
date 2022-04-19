e5a9c5cb01a21f50d4ae5527a79f591c5659b064
integral <mohammed.a.tazwar04@gmail.com>
2022-04-19 18:34:34 -0400

    fixed dms not working

--- src/events.rs
+++ src/events.rs
@@ -2,6 +2,7 @@ use serenity::async_trait;
 use serenity::model::gateway::Ready;
 use serenity::model::id::GuildId;
 use serenity::model::prelude::Message;
+use serenity::model::channel::ChannelType;
 use serenity::prelude::*;
 use serenity::utils::MessageBuilder;
 use  serenity::model::id::ChannelId;
@@ -19,13 +20,31 @@ impl EventHandler for Handler {
     }
 
     async fn message(&self, context: Context, msg: Message) {
-        const CLIENT_ID: u64 = 965142516141789216;
-        const CHANNEL_ID: u64 = 965109206468395049;
-        const DM_ID: u64 = 965749825444397067;
         if msg.author.id == CLIENT_ID {
             return;
         }
-        if msg.channel_id == CHANNEL_ID || msg.channel_id == DM_ID {
+
+        const CLIENT_ID: u64 = 965142516141789216;
+        const CHANNEL_ID: u64 = 965109206468395049;
+        let mut is_sus: bool = false;
+        let mut is_dms: bool = false;
+        
+        if msg.channel_id == CHANNEL_ID {
+            is_sus = true;
+        } 
+        let channel = ChannelId(*msg.channel_id.as_u64()).to_channel(&context.http).await.unwrap();
+        match channel.private() {
+            Some(private) => { 
+                is_sus = true; 
+                is_dms = true;
+            },
+            None => {},
+        }
+
+        if is_sus {
+            if !is_dms {
+                msg.delete(&context).await;
+            }
             let channel = match msg.channel_id.to_channel(&context).await {
                 Ok(channel) => channel,
                 Err(err) => {
@@ -34,7 +53,6 @@ impl EventHandler for Handler {
                 },
             };
             let msg_content: String = msg.content.clone();
-            msg.delete(&context).await;
             let rand_id: i32 = rand::thread_rng().gen_range(100000,999999);
             let mut return_msg = MessageBuilder::new()
                 .push("[")
@@ -42,12 +60,12 @@ impl EventHandler for Handler {
                 .push("] ")
                 .push(msg_content)
                 .build();
-            for i in &msg.attachments {
-                ChannelId(CHANNEL_ID).say(&context.http, &i.url).await;
-            }
             if let Err(err) = ChannelId(CHANNEL_ID).say(&context.http, &return_msg).await {
                 println!("Error sending message: {:?}", err);
                 return;
+            }
+            for i in &msg.attachments {
+                ChannelId(CHANNEL_ID).say(&context.http, &i.url).await;
             }
         }
     }
